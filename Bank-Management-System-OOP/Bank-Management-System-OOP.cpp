19dca3e51b22fc3b5fcea18f81413bb9aa81e529
pc <pc@FURY>
2026-09-23 14:30:10 +0300

    Add project files.

---
+++ Bank-Management-System-OOP/Bank-Management-System-OOP.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "LoginScreen.h"
+
+using namespace std;
+
+
+int main()
+{
+
+    while (true)
+    {
+        if (!LoginScreen::ShowLoginScreen())
+        {
+            break;
+        }
+
+    }
+
+
+    return 0;
+}
---
+++ Bank-Management-System-OOP/Global.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "BankUser.h"
+
+BankUser CurrentUser = BankUser::Find("", "");
